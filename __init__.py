3259fab3f0e30266e18636719a57db6c6f932494
Che5hireC4t <matthieu.epiard@protonmail.com>
2023-08-18 15:07:31 +0200

    Adding a docstring describing the multiprocessor module.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,46 @@
-from sys import path
+"""
+`multiprocessor` Module
+=======================
 
-__here_path = '/'.join(__file__.split('/')[:-1])
-path.append(__here_path) if __here_path not in path else None
+This module provides tools to manage and execute tasks, both in parallel and incrementally.
+The primary components of the module are the `Job` and `Multiprocessor` classes.
 
-from Job import Job
-from Multiprocessor import Multiprocessor
+The `Job` class allows you to define, schedule, and manage lists of tasks. It provides
+exception handling and fallback task mechanisms to ensure that tasks can be recovered or
+alternative actions can be taken when something unexpected occurs. Each job maintains
+a list of tasks to be executed, a list of exceptions to handle, and a list of fallback tasks
+to run if any exceptions are encountered.
+
+The `Multiprocessor` class handles the parallelization (or not) of independent pieces of code.
+It leverages the `multiprocessing` library to distribute tasks into concurrent subprocesses.
+However, it provides a flexible mechanism to choose between parallel execution and incremental
+execution in a single process. By switching a boolean parameter, users can decide how tasks
+should be executed, offering flexibility in terms of resources and debugging.
+
+Additionally, the `Multiprocessor` class is designed to work seamlessly with the `Job` class.
+Users can define a series of jobs and then pass them to the `Multiprocessor` for execution.
+
+Main Components
+---------------
+1. `Task`: A class that represents a task containing a callable and its arguments.
+2. `Job`: A class that encapsulates a list of tasks to be executed. Offers exception handling and fallback mechanisms.
+3. `Multiprocessor`: A class that provides mechanisms for executing tasks,
+                     either in parallel using subprocesses or incrementally.
+
+Usage Examples:
+---------------
+1. Define tasks using the `Job` class.
+2. Define whether tasks should be run in parallel or incrementally using the `Multiprocessor` class.
+3. Execute the jobs and retrieve the results.
+
+You can view a fully functional example in the docstring of `Multiprocessor` class
+
+Note: The `Multiprocessor` class is designed to work seamlessly with instances of the `Job` class.
+This module provides both parallel and incremental execution capabilities, making it versatile for various scenarios.
+
+For detailed examples and further documentation, please refer to the docstrings of the respective classes and methods.
+"""
+
+from .Task import Task
+from .Job import Job
+from .Multiprocessor import Multiprocessor
